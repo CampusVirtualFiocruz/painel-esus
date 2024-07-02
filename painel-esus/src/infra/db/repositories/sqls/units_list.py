23258916e6035f6278fb19762cfe03a8ca3124c6
Tales Mota <talesmmachado@gmail.com>
2024-07-02 15:43:33 -0300

    Ajuste na query de UBS. Ordenando alfabeticamente e selecionando ubs com atendimentos

--- painel-esus/src/infra/db/repositories/sqls/units_list.py
+++ painel-esus/src/infra/db/repositories/sqls/units_list.py
@@ -7,5 +7,14 @@ from tb_dim_unidade_saude a
 where exists (
       select 1 from tb_fat_atendimento_individual b
       where b.co_dim_unidade_saude_1 = a.co_seq_dim_unidade_saude 
-); 
+      and b.co_dim_tempo::text::date between (
+		select
+			max(co_dim_tempo)
+		from
+			tb_fat_atendimento_individual)::text::DATE - interval '12 month' and (
+		select
+			max(co_dim_tempo)
+		from
+			tb_fat_atendimento_individual)::text::DATE
+) order by no_unidade_saude asc; 
 """
