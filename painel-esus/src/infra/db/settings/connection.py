fca27d02eb4d8fd7e3121d687fc72c73eea1d49f
Tales Mota Machado <talesmmachado@gmail.com>
2024-07-05 10:10:19 -0300

    refactor: Ajuste na conexao, colocando encode no password (#20)

--- painel-esus/src/infra/db/settings/connection.py
+++ painel-esus/src/infra/db/settings/connection.py
@@ -1,32 +1,41 @@
+# pylint: disable=R0913
+from urllib.parse import quote_plus
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.env.conf import env
-import urllib
 
 
 class DBConnectionHandler:
 
-    def __init__(self, dbUser = None, dbPassword = None, dbHost = None, dbPort = None, dbDatabase = None) -> None:
+    def __init__(
+        self,
+        db_user=None,
+        db_password=None,
+        db_host=None,
+        db_port=None,
+        db_database=None,
+    ) -> None:
         self.__connection_string = "{}://{}:{}@{}:{}/{}".format(
-            'postgresql',
-            env.get('DB_USER', '-') if dbUser is None else dbUser.strip(),
-            env.get('DB_PASSWORD', '-') if dbPassword is None else dbPassword.strip(),
-            env.get('DB_HOST', '-') if dbHost is None else dbHost.strip(),
-            env.get('DB_PORT', '-') if dbPort is None else dbPort.strip(),
-            env.get('DB_DATABASE', '-') if dbDatabase is None else dbDatabase.strip()
+            "postgresql",
+            env.get("DB_USER", "-") if db_user is None else db_user.strip(),
+            (
+                quote_plus(env.get("DB_PASSWORD", "-"))
+                if db_password is None
+                else quote_plus(db_password.strip())
+            ),
+            env.get("DB_HOST", "-") if db_host is None else db_host.strip(),
+            env.get("DB_PORT", "-") if db_port is None else db_port.strip(),
+            env.get("DB_DATABASE", "-") if db_database is None else db_database.strip(),
         )
         self.__engine = self.__create_database_engine()
-        print('+++++++++++++++++++++++++++++++',self.__engine)
         self.session = None
-    
 
     def __create_database_engine(self):
         engine = create_engine(self.__connection_string)
         return engine
-    
+
     def get_engine(self):
-        print("TESTE DE CONEXÃO")
-        print( self.__connection_string)
         return self.__engine
 
     def __enter__(self):
