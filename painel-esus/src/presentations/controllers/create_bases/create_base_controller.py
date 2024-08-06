057f5ba19ca5c6fac5df8c07abb5acdf34b12778
Tales Mota <talesmmachado@gmail.com>
2024-08-06 14:42:35 -0300

    Ajustes

--- painel-esus/src/presentations/controllers/create_bases/create_base_controller.py
+++ painel-esus/src/presentations/controllers/create_bases/create_base_controller.py
@@ -1,5 +1,9 @@
 # pylint: disable=E0401,C0301,W0612,W0611
+import os
+from pathlib import Path
+
 from src.data.use_cases.create_bases.create_bases_usecase import CreateBasesUseCase
+from src.env import env
 from src.errors.logging import logging
 from src.infra.create_base.create_diabetes_bases_repository import (
     CreateDiabetesBasesRepository,
@@ -18,6 +22,14 @@ from src.infra.create_base.create_smoking_bases_repository import (
 class CreateBasesController:
 
     def create_bases(self):
+        if os.getenv("ENV") == "instalador":
+            path = 'painel_esus.db'
+        else:
+            path = os.getcwd()
+            path = Path(path.split('/painel-esus')[0])
+            path = os.path.join(path, 'painel_esus.db')
+            path = os.path.relpath(path)
+        os.remove(path)
         logging.info("Starting base generation")
         _list = [
             CreateDiabetesBasesRepository(),
