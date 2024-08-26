cd04c77e5d44cc96ddeba4d115a0aa9d8be96783
Igor Caldeira <igorcaldeiraa@gmail.com>
2024-08-26 09:41:10 -0300

    retorna autenticação

--- paineis-v2-front/src/components/ProtectedLayout/index.tsx
+++ paineis-v2-front/src/components/ProtectedLayout/index.tsx
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider/useAuth";
 
 export const ProtectedLayout = ({ children }: { children: JSX.Element }) => {
-  // const auth = useAuth();
+  const auth = useAuth();
 
-  // if (!auth.username) {
-  //     return (
-  //         <>
-  //             <h1>Você não tem permissão para acessar esta página.</h1>
+  if (!auth.username) {
+    return (
+      <>
+        <h1>Você não tem permissão para acessar esta página.</h1>
 
-  //             <Link to="/">Fazer login</Link>
-  //         </>
-  //     )
-  // }
+        <Link to="/">Fazer login</Link>
+      </>
+    );
+  }
 
   return children;
 };
