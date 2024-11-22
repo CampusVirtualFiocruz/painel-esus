af7e6c32e07932a57f5450f95ec8c938aa9a98e2
Tales Mota <talesmmachado@gmail.com>
2024-11-22 08:47:59 -0300

    Ajuste na barra de processo

--- painel-esus/src/data/use_cases/create_bases/create_bases_usecase.py
+++ painel-esus/src/data/use_cases/create_bases/create_bases_usecase.py
@@ -4,10 +4,10 @@ from src.data.interfaces.create_bases.create_bases_repository import (
     CreateBasesRepositoryInterface,
 )
 from src.domain.use_cases.create_bases.create_bases import CreateBasesUsecasesInterface
-from src.errors import InvalidArgument
-from src.errors import NoSuchTableError
+from src.errors import InvalidArgument, NoSuchTableError
 from src.errors.logging import logging
 from tqdm import tqdm
+
 
 class CreateBasesUseCase(CreateBasesUsecasesInterface):
 
@@ -20,12 +20,8 @@ class CreateBasesUseCase(CreateBasesUsecasesInterface):
     def create_bases(self):
         if self.__bases_generators is None:
             raise InvalidArgument('No bases to generate was passed.')
-        for base in tqdm(self.__bases_generators):
+        for base in tqdm(self.__bases_generators, ascii="░▒█", desc="Geração das Bases: "):
             if isinstance(base, CreateBasesRepositoryInterface):
-                # try:
-                #     base.destroy_base()
-                # except NoSuchTableError:
-                #     logging.info("Base already destroyed!")
                 base.create_base()
             else:
                 raise InvalidArgument(
