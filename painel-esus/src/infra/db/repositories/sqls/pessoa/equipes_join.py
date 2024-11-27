128056b71f861d1c57db6616df9f3412110edb0a
Tales Mota <talesmmachado@gmail.com>
2024-11-27 14:59:08 -0300

    Ajuste no dados de graficos de pizza de cadatros

--- painel-esus/src/infra/db/repositories/sqls/pessoa/equipes_join.py
+++ painel-esus/src/infra/db/repositories/sqls/pessoa/equipes_join.py
@@ -13,6 +13,7 @@ select distinct
 			left join tb_dim_unidade_saude tdus on tdus.co_seq_dim_unidade_saude = ce.co_dim_unidade_saude
 			left join tb_dim_equipe tde on tde.co_seq_dim_equipe = ce.co_dim_equipe
 			join pessoas_id p on p.co_seq_fat_cidadao_pec = ce.co_fat_cidadao_pec
+		where nu_micro_area is not null
 		order by
 			co_fat_cidadao_pec asc,
 			co_dim_unidade_saude asc,
