c16c298c810c1f5db4181b3957fd6045508cc592
igorcaldeira <igorcaldeiraa@gmail.com>
2025-02-18 00:07:18 -0300

    deploy docs

---
+++ docs/build/assets/js/5e95c892.0bf46314.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[9647],{4951:(e,s,r)=>{r.r(s),r.d(s,{default:()=>l});r(758);var t=r(3526),u=r(6287),a=r(1005),c=r(389),n=r(5861),i=r(6070);function l(e){return(0,i.jsx)(u.e3,{className:(0,t.A)(a.G.wrapper.docsPages),children:(0,i.jsx)(n.A,{children:(0,c.v)(e.route.routes)})})}}}]);
