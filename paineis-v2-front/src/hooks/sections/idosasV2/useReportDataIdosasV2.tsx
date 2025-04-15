222e1caed26c536287ea5e78ad55c6048884da69
igorcaldeira <igorcaldeiraa@gmail.com>
2025-04-15 19:12:23 -0300

    fix ubs param on elderly

--- paineis-v2-front/src/hooks/sections/idosasV2/useReportDataIdosasV2.tsx
+++ paineis-v2-front/src/hooks/sections/idosasV2/useReportDataIdosasV2.tsx
@@ -10,58 +10,78 @@ const useReportDataIdosasV2 = ({ ubsId, squadId }: reportBasicInfo) => {
   return useQuery(
     ["relatorio-idoso", ubsId, squadId],
     async () => {
+      const ubsParam = ubsId ? `/${ubsId}` : "";
+
       const requests = {
-        "total-ubs": Api.get(`/elderly/total-ubs`, {
+        "total-ubs": Api.get(`/elderly/total-ubs${ubsParam}`, {
           params: {
             equipe: squadId,
           },
         }),
-        "total-atendidas": Api.get(`/elderly/total-medical-cares`, {
+        "total-atendidas": Api.get(`/elderly/total-medical-cares${ubsParam}`, {
           params: {
             equipe: squadId,
           },
         }),
-        "pessoas-por-sexo": Api.get(`/elderly/by-gender`, {
+        "pessoas-por-sexo": Api.get(`/elderly/by-gender${ubsParam}`, {
           params: {
             equipe: squadId,
           },
         }),
-        "distribuicao-pessoas-raca-cor": Api.get(`/elderly/by-race`, {
+        "distribuicao-pessoas-raca-cor": Api.get(
+          `/elderly/by-race${ubsParam}`,
+          {
+            params: {
+              equipe: squadId,
+            },
+          }
+        ),
+        "duas-consultas-medicas-enfermagem": Api.get(
+          `/elderly/two-acs-visits${ubsParam}`,
+          {
+            params: {
+              equipe: squadId,
+            },
+          }
+        ),
+        "dois-registros-peso-altura": Api.get(
+          `/elderly/two-height-records${ubsParam}`,
+          {
+            params: {
+              equipe: squadId,
+            },
+          }
+        ),
+        "duas-visitas-domiciliares-acs-tacs": Api.get(
+          `/elderly/two-acs-visits${ubsParam}`,
+          {
+            params: {
+              equipe: squadId,
+            },
+          }
+        ),
+        "avalicao-creatina": Api.get(`/elderly/creatinine${ubsParam}`, {
           params: {
             equipe: squadId,
           },
         }),
-        "duas-consultas-medicas-enfermagem": Api.get(`/elderly/two-acs-visits`, {
-          params: {
-            equipe: squadId,
-          },
-        }), 
-        "dois-registros-peso-altura": Api.get(`/elderly/two-height-records`, {
-          params: {
-            equipe: squadId,
-          },
-        }),
-        "duas-visitas-domiciliares-acs-tacs": Api.get(`/elderly/two-acs-visits`, {
-          params: {
-            equipe: squadId,
-          },
-        }),
-        "avalicao-creatina": Api.get(`/elderly/creatinine`, {
-          params: {
-            equipe: squadId,
-          },
-        }),
-        "registro-vacina-influenza": Api.get(`/elderly/influenza-vaccines`, {
-          params: {
-            equipe: squadId,
-          },
-        }),
-        "consulta-com-dentista-aps": Api.get(`/elderly/dentist-appointment`, {
-          params: {
-            equipe: squadId,
-          },
-        }),
-        "ivcf-20": Api.get(`/elderly/ivcf-20`, {
+        "registro-vacina-influenza": Api.get(
+          `/elderly/influenza-vaccines${ubsParam}`,
+          {
+            params: {
+              equipe: squadId,
+            },
+          }
+        ),
+        "consulta-com-dentista-aps": Api.get(
+          `/elderly/dentist-appointment${ubsParam}`,
+          {
+            params: {
+              equipe: squadId,
+            },
+          }
+        ),
+        "ivcf-20": Api.get(`/elderly/ivcf-20${ubsParam}`, {
           params: {
             equipe: squadId,
           },
