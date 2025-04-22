c61544209fb76dd792d16363801c4b0606dffab0
Tales Mota <talesmmachado@gmail.com>
2025-04-22 16:06:27 -0300

    Conversao para fastparquet

--- painel-esus/src/infra/create_base/polars/create_fat_ivcf_repository.py
+++ painel-esus/src/infra/create_base/polars/create_fat_ivcf_repository.py
@@ -4,68 +4,25 @@ from sqlalchemy import text
 import pyarrow as pa
 import pyarrow.parquet as pq
 from src.env.conf import getenv
+from src.infra.create_base.polars.abstract_generate_base import AbstractGenerateBase
 
-lista_vars = ['co_seq_fat_ivcf','co_fat_cidadao_pec','co_dim_tempo']
-lista_vars_str = ', '.join(lista_vars)
-
-SQL = f"SELECT {lista_vars_str} FROM tb_fat_ivcf order by co_seq_fat_ivcf"
-
-class CreatIvcfBaseRepository:
+class CreatIvcfBaseRepository(AbstractGenerateBase):
     _base = 'tb_fat_ivcf'
 
+
     def __init__(self):
-        ...
+        lista_vars = ['co_seq_fat_ivcf','co_fat_cidadao_pec','co_dim_tempo']
+        
+        dtype = {
+            'co_seq_fat_ivcf': pd.Int64Dtype(),
+            'co_fat_cidadao_pec': pd.Int64Dtype(),
+            'co_dim_tempo': pd.Int64Dtype()
+        }
+        self._sql = "SELECT {} FROM tb_fat_ivcf order by co_seq_fat_ivcf".format(", ".join(lista_vars))
+        super().__init__( DBConnectionHandler(), self._sql, dtype)
+        
 
     def get_base(self):
         return self._base
 
-    def create_base(self):
-        try:
-
-            schema_fixo =  self.get_schema() 
-            _next = True
-            offset = 0
-            chunk_size = getenv("CHUNK_SIZE", 2500000)
-            parquet_file = f"{self._base}.parquet"
-            writer = None 
-            while _next:
-                with DBConnectionHandler() as db:
-                    engine = db.get_engine()
-                    print(text(f"{SQL}  LIMIT {chunk_size} OFFSET {offset};"))
-                    df = pd.read_sql_query(
-                        text(f'{SQL}  LIMIT {chunk_size} OFFSET {offset};'), con=engine,dtype_backend='pyarrow')
-
-                    if df.shape[0] is not None and df.shape[0] > 0:
-                        _next = True
-                    else:
-                        _next = False
-
-                    offset += chunk_size
-
-                    if not df.empty:
-                        table = pa.Table.from_pandas(df,schema = schema_fixo,preserve_index = False)
-
-                        if writer is None:
-
-                            writer = pq.ParquetWriter("dados/input/"+parquet_file,schema_fixo) #, schema=schema_fixo
-                        writer.write_table(table)
-
-            if writer:
-                writer.close()  
-        except Exception as e:
-            print(e)
-            print(f'Erro {self._base} already destroyed!')
-
-
-
-    def get_schema(self):
-         # Definindo o schema fixo
-        schema = pa.schema([
-            pa.field('co_seq_fat_ivcf', pa.int64(), nullable=False),
-            pa.field('co_dim_tempo', pa.int64(), nullable=True),
-            pa.field('co_fat_cidadao_pec', pa.int64(), nullable=True),
-
-        ])
-        return schema
-
-
+    
