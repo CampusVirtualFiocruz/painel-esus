a327d86960a7511fd8ab7e77e7fcb56d4d79ba32
Tales Mota <talesmmachado@gmail.com>
2025-06-24 18:16:52 -0300

    Ajuste no objeto sim nao nao se aplica

--- painel-esus/src/main/adapters/children_adapter.py
+++ painel-esus/src/main/adapters/children_adapter.py
@@ -6,14 +6,33 @@ class ChildrenAdapter:
     def _to_tag_value_list(self, response):
         return {"data": [{"tag": tag, "value": value} for tag, value in response]}
 
+    def _to_tag_value_list_sim_nao(self, response):
+        data = {
+            "sim": {
+                "tag": "sim",
+                "value": 0,
+            },
+            "nao": {
+                "tag": "nao",
+                "value": 0,
+            },
+            "nao-se-aplica": {
+                "tag": "nao-se-aplica-infantil",
+                "value": 0,
+            },
+        }
+        for tag, value in response:
+            data[tag]["value"] = value
+        return {"data": list(data.values())}
+
     def by_race_children(self, response):
         return self._to_tag_value_list(response)
 
     def first_consult_8d(self, response):
-        return self._to_tag_value_list(response)
+        return self._to_tag_value_list_sim_nao(response)
 
     def appointments_until_2_years(self, response):
-        return self._to_tag_value_list(response)
+        return self._to_tag_value_list_sim_nao(response)
 
     def by_age_children(self, response):
         data = []
@@ -33,16 +52,16 @@ class ChildrenAdapter:
         return {"data": data}
 
     def acs_visit_until_30d(self, response):
-        return self._to_tag_value_list(response)
+        return self._to_tag_value_list_sim_nao(response)
 
     def acs_visit_until_6m(self, response):
-        return self._to_tag_value_list(response)
+        return self._to_tag_value_list_sim_nao(response)
 
     def dental_appointments_until_12m(self, response):
-        return self._to_tag_value_list(response)
+        return self._to_tag_value_list_sim_nao(response)
 
     def dental_appointments_until_24m(self, response):
-        return self._to_tag_value_list(response)
+        return self._to_tag_value_list_sim_nao(response)
 
     def high_weight_records(self, response):
         return self._to_tag_value_list(response)
