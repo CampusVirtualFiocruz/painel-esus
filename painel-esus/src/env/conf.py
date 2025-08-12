284a4883b9e80a0ad3bc4f2af50c0ced56e3aba7
AdrianoNevesFoo <adrianonevesps@gmail.com>
2025-08-12 09:59:26 -0300

    automatizando processo de geração de versão

--- painel-esus/src/env/conf.py
+++ painel-esus/src/env/conf.py
@@ -2,9 +2,12 @@ import os
 
 from dotenv import dotenv_values
 
+VERSION_PAINEL = "${VERSION}"
+
 env = {
     **dotenv_values(os.path.abspath(".env")),  # load shared development variables
     **os.environ,  # override loaded values with environment variables
+    "APPLICATION_VERSION": VERSION_PAINEL,
 }
 
 def getenv(key, default, numeric=True):
